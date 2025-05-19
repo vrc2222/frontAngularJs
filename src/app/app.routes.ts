f420199a389c5d4e8e774c0b5c189b72376c09a3
vrc2222 <204788805+vrc2222@users.noreply.github.com>
2025-05-19 08:53:53 -0500

    ya hicimos todo el front falta los servicos al back

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent} from './auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path:'home',component:HomeComponent, canActivate:[authGuard]},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+];
